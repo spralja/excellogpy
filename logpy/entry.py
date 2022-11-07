ab039b8fc3fef4adc2c62b9035e4b39706de6684
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-11-07 18:27:08 +0100

    Docs: Entry

--- logpy/entry.py
+++ logpy/entry.py
@@ -3,17 +3,26 @@ from typing import Tuple
 
 
 class Entry:
+    """
+    Class for log entries
+    """
     start_time: datetime
     end_time: datetime
     category: str
     description: str
 
-    def __init__(self, start_time, end_time, category, description):
+    def __init__(self, start_time, end_time, category, description=''):
         pass
 
     @property
     def duration(self) -> timedelta:
-        pass
+        """
+        Returns the duration of the entry
+        :return: duration
+        """
 
     def to_pair(self) -> Tuple[str, timedelta]:
-        pass
+        """
+        Returns a tuple of the form (category, duration)
+        :return:
+        """
