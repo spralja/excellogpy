f9920a796b7c40dc79c94888a10ee31ff403e28d
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-11-07 18:23:32 +0100

    Declared Entry

--- logpy/entry.py
+++ logpy/entry.py
@@ -1,4 +1,19 @@
+from datetime import datetime, timedelta
+from typing import Tuple
 
 
 class Entry:
-    pass
+    start_time: datetime
+    end_time: datetime
+    category: str
+    description: str
+
+    def __init__(self, start_time, end_time, category, description):
+        pass
+
+    @property
+    def duration(self) -> timedelta:
+        pass
+
+    def to_pair(self) -> Tuple[str, timedelta]:
+        pass
