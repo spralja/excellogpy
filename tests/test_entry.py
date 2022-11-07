ba8a547e6bc8367000b030aa2a7eae2935c11172
Robert Spralja <56681655+spralja@users.noreply.github.com>
2022-11-07 18:11:55 +0100

    Chore

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,28 +1,30 @@
 import unittest
 from logpy.entry import Entry
-from datetime import datetime
+from datetime import datetime, timedelta
+
+start_time = datetime(2022, 11, 7, 17)
+end_time = datetime(2022, 11, 7, 18)
+category = 'Category'
+description = 'Description'
+
+duration = timedelta(0, 1)
 
 
 class ConstructorTestCase(unittest.TestCase):
-    start_time = datetime(2022, 11, 7, 17)
-    end_time = datetime(2022, 11, 7, 18)
-    category = 'Category'
-    description = 'Description'
-
     def test00(self):
-        entry = Entry(self.start_time, self.end_time, self.category, self.description)
-        self.assertEqual(entry.start_time, self.start_time)
-        self.assertEqual(entry.end_time, self.end_time)
-        self.assertEqual(entry.category, self.category)
-        self.assertEqual(entry.description, self.description)
+        entry = Entry(start_time, end_time, category, description)
+        self.assertEqual(entry.start_time, start_time)
+        self.assertEqual(entry.end_time, end_time)
+        self.assertEqual(entry.category, category)
+        self.assertEqual(entry.description, description)
 
     def test01(self):
-        entry = Entry(self.start_time, self.end_time, self.category)
-        self.assertEqual(entry.start_time, self.start_time)
-        self.assertEqual(entry.end_time, self.end_time)
-        self.assertEqual(entry.category, self.category)
+        entry = Entry(start_time, end_time, category)
+        self.assertEqual(entry.start_time, start_time)
+        self.assertEqual(entry.end_time, end_time)
+        self.assertEqual(entry.category, category)
         self.assertEqual(entry.description, '')
 
     def test02(self):
         with self.assertRaises(ValueError):
-            entry = Entry(self.end_time, self.start_time, self.category, self.description)
+            entry = Entry(end_time, start_time, category, description)
